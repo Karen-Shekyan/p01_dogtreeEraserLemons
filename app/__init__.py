c1ef498821b0e4c67f4b58cce443fb8b35690004
Kevin Liu <kevinliu@kevins-air-2.78m475.nycboe.org>
2022-12-09 09:22:15 -0500

    added basic information display for hero

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, session, request
-import requests, os
+import requests, os, json
 import database
 app = Flask(__name__)
 secret_key = os.urandom(32)
@@ -20,6 +20,59 @@ def authenticate():
         pw = request.args['pass']
 
     return render_template('login.html')
+@app.route('/hero/<int:hero_id>')
+def display(hero_id):
+    url = f"https://akabab.github.io/superhero-api/api/id/{hero_id}.json" 
+    print(url)  
+    data = json.loads(requests.get(url).text)
+    print(data)
+    print("----------------------------------")
+    FullName = data["biography"]["fullName"]
+    name = data["name"]
+    powerstats = data["powerstats"]
+    alignedment = data["biography"]["alignment"]
+    placeOfBirth = data["biography"]["placeOfBirth"]
+    if placeOfBirth == "-":
+        placeOfBirth = ""
+    else: 
+        placeOfBirth = "borned in "+placeOfBirth
+    gender = data["appearance"]["gender"]
+    if(gender == "Male"):
+        pronoun = "He"
+    else:
+        pronoun = "she"
+    race = data["appearance"]["race"]
+    height = data["appearance"]["height"][0]
+    if height != "-":
+        height = "with the height of "+ height + " "
+    else:
+        height = ""
+    if race == None:
+        race = ""
+    else:
+        race = race + " "
+    weight = data["appearance"]["weight"][0]
+    if weight == "- lb":
+        weight = ""
+    else:
+        weight = "and weighing " + weight + " "
+    eyeColor = data["appearance"]["eyeColor"]
+    hairColor = data["appearance"]["hairColor"]
+    if hairColor != "No Hair":
+        hairColor = hairColor + " hair"
+    if eyeColor == "-":
+        eyeColor = "No"
+    occupation = data["work"]["occupation"]
+    firstAppearance = data["biography"]["firstAppearance"]
+    publisher = data["biography"]["publisher"]
+    groupAffiliation = data["connections"]["groupAffiliation"]
+    if groupAffiliation == "-":
+        groupAffiliation = "with no one"
+    else:
+        groupAffiliation = "as a" + groupAffiliation
+    bio = f'{FullName} or {name} is a {alignedment} aligned character {placeOfBirth}. {name} has the appearance of a {race}{gender} {height} {weight}with {eyeColor} eyes and {hairColor}. {pronoun} works as a {occupation}. {pronoun} first appeared in {firstAppearance} by {publisher}. {pronoun} is affiliated {groupAffiliation}.'
+    image = data["images"]["md"]
+    return render_template('hero.html', Information = bio, picture = image, stats = [powerstats])
 
 if __name__ == '__main__':
 	app.debug = True
